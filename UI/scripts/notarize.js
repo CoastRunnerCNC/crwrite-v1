ee7db85c11d3f5e88697fd478c249da09555a59c
Max <mrheffern@gmail.com>
2024-05-28 16:01:15 -0700

    updating notarize process

--- UI/scripts/notarize.js
+++ UI/scripts/notarize.js
@@ -1,19 +1,25 @@
 require('dotenv').config();
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName != 'darwin') {
+  if (electronPlatformName !== 'darwin') {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: 'org.coastrunner.crwrite',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-    teamId: process.env.TEAMID,
-  });
+  try {
+    await notarize({
+      appBundleId: 'org.coastrunner.crwrite',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
+      teamId: process.env.TEAMID,
+    });
+    console.log('Notarization successful');
+  } catch (error) {
+    console.error('Notarization failed:', error);
+    throw error; // Rethrow the error to handle it upstream if necessary
+  }
 };

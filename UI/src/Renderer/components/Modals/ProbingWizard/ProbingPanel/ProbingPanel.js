15c8dea1f0477201bf240fe5aac35f223e4df818
Max <mrheffern@gmail.com>
2024-06-06 09:44:09 -0700

    adding missed files

---
+++ UI/src/Renderer/components/Modals/ProbingWizard/ProbingPanel/ProbingPanel.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ProbingPanel = (props) => {
+    return (
+        <ItemPanel small title="Settings">
+            <Grid
+                container
+                spacing={2}
+                direction="column"
+                style={{
+                    paddingTop: "16px",
+                    paddingBottom: "16px",
+                }}
+                alignContent="center"
+            >
+                <Grid item xs={10}>
+
+                </Grid>
+                <Grid item xs={10}>
+
+                </Grid>
+                <Grid item xs={10}>
+
+                </Grid>
+            </Grid>
+        </ItemPanel>
+    );
+};
+
+export default ProbingPanel;

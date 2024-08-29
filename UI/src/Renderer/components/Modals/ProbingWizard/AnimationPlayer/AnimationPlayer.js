595633f924bcbbc9353bf8d6c62e65ce38552884
Max <mrheffern@gmail.com>
2024-08-28 23:15:02 -0700

    adding basic layout of dual animation player for probing wizard

--- UI/src/Renderer/components/Modals/ProbingWizard/AnimationPlayer/AnimationPlayer.js
+++ UI/src/Renderer/components/Modals/ProbingWizard/AnimationPlayer/AnimationPlayer.js
@@ -1,50 +1,170 @@
 import React, { useState, useEffect, useRef } from "react";
+import { withStyles } from "@material-ui/core";
+
+const styles = (theme) => ({
+    container: {
+        display: "grid",
+        width: "100%",
+        height: "100%",
+    },
+    video: {
+        gridArea: "1 / 1 / 2 / 2",
+    },
+    secondVideoPlayer: {
+        zIndex: 1,
+        gridArea: "1 / 1 / 2 / 2",
+    },
+    firstVideoPlayer: {
+        zIndex: 2,
+        gridArea: "1 / 1 / 2 / 2",
+    },
+});
 
 const AnimationPlayer = (props) => {
     const animations = props.animations;
 
     const videoRef = useRef(null);
+    const videoRef2 = useRef(null);
     const animationIndexRef = useRef(0);
     const repeatCountRef = useRef(0);
 
     useEffect(() => {
         const videoElement = videoRef.current;
-
-        const playNextAnimation = () => {
-            if (animationIndexRef.current >= animations.length) {
-                animationIndexRef.current = 0;
-            }
-            const currentAnimation = animations[animationIndexRef.current];
-            videoElement.src = currentAnimation.src;
-            videoElement.playbackRate = currentAnimation.speed;
-            videoElement.play();
-            repeatCountRef.current += 1;
-        };
+        const videoElement2 = videoRef2.current;
+        let activeVideo = videoElement;
+        let inactiveVideo = videoElement2;
+        let currentAnimation = animations[animationIndexRef.current];
+        let nextAnimation;
 
         const handleVideoEnd = () => {
-            const currentAnimation = animations[animationIndexRef.current];
-            if (repeatCountRef.current >= currentAnimation.repeat) {
-                videoElement.pause();
-                setTimeout(() => {
-                    repeatCountRef.current = 0;
-                    animationIndexRef.current += 1;
-                    playNextAnimation();
-                }, 3000);
+
+            if (animationIndexRef.current + 1 === animations.length) {
+                animationIndexRef.current = 0;
             } else {
-                playNextAnimation();
+                ++animationIndexRef.current;
             }
+            currentAnimation = animations[animationIndexRef.current];
+
+            if (animationIndexRef.current + 1 === animations.length) {
+                nextAnimation = animations[0];
+            } else {
+                nextAnimation = animations[animationIndexRef.current + 1];
+            }
+
+            // cleanup old listener
+            activeVideo.removeEventListener("ended", handleVideoEnd);
+
+            // when video ends, set activeVideo to other videoElement
+            activeVideo = activeVideo === videoElement ? videoElement2 : videoElement;
+            inactiveVideo = activeVideo === videoElement ? videoElement2 : videoElement;
+            
+            // attach new listener to active video
+            activeVideo.addEventListener("ended", handleVideoEnd);
+            
+            // setup all video props
+            activeVideo.src = currentAnimation.src;
+            activeVideo.playbackRate = currentAnimation.speed;
+            
+            // move inactive video to active video index plus 1 unless activeVideo is at end of array
+            inactiveVideo.src = nextAnimation.src;
+            inactiveVideo.playbackRate = nextAnimation.speed;
+
+
+            // start transition
+
+            
+            // play active video
+            activeVideo.play();
         };
 
-        videoElement.addEventListener("ended", handleVideoEnd);
+        activeVideo.addEventListener("ended", handleVideoEnd);
+        activeVideo.src = currentAnimation.src;
+        activeVideo.playbackRate = currentAnimation.speed;
 
-        playNextAnimation(); // Start the first animation
+        activeVideo.play();
 
         return () => {
             videoElement.removeEventListener("ended", handleVideoEnd);
+            videoElement2.removeEventListener("ended", handleVideoEnd);
         };
+
+        /////////////////////////////////////////////////////////////////////////
+
+        // const videoElement = videoRef.current;
+        // const videoElement2 = videoRef2.current;
+
+        // const playNextAnimation = () => {
+        //     if (animationIndexRef.current >= animations.length) {
+        //         animationIndexRef.current = 0;
+        //     }
+
+        //     const currentAnimation = animations[animationIndexRef.current];
+        //     let nextAnimation;
+
+        //     if ((animationIndexRef.current + 1) === animations.length) {
+        //         nextAnimation = animations[0];
+        //     } else {
+        //         nextAnimation = animations[animationIndexRef.current + 1];
+        //     }
+        //     // play first animation
+
+        //     videoElement.src = currentAnimation.src;
+        //     videoElement.playbackRate = currentAnimation.speed;
+        //     videoElement.play();
+
+        //     // setup second animation but don't play
+
+        //     videoElement2.src = nextAnimation.src;
+        //     videoElement2.playbackRate = nextAnimation.speed;
+
+        //     repeatCountRef.current += 1;
+        // };
+
+        // const handleVideoEnd = () => {
+        //     // switch z-index of animations
+
+        //     // playNextAnimation
+
+        //     const currentAnimation = animations[animationIndexRef.current];
+        //     if (repeatCountRef.current >= currentAnimation.repeat) {
+        //         videoElement.pause();
+        //         setTimeout(() => {
+        //             repeatCountRef.current = 0;
+        //             animationIndexRef.current += 1;
+        //             playNextAnimation();
+        //         }, 3000);
+        //     } else {
+        //         playNextAnimation();
+        //     }
+        // };
+
+        // videoElement.addEventListener("ended", handleVideoEnd);
+
+        // playNextAnimation(); // Start the first animation
+
+        // return () => {
+        //     videoElement.removeEventListener("ended", handleVideoEnd);
+        // };
     }, []);
 
-    return <video style={{maxWidth: "100%"}} ref={videoRef} muted />;
+    return (
+        <>
+            <div className={props.classes.container}>
+                <video
+                    className={props.classes.firstVideoPlayer}
+                    style={{ maxWidth: "100%" }}
+                    ref={videoRef}
+                    muted
+                />
+                <video
+                    className={props.classes.secondVideoPlayer}
+                    style={{ maxWidth: "100%" }}
+                    ref={videoRef2}
+                    muted
+                />
+            </div>
+        </>
+    );
 };
 
-export default AnimationPlayer;
+export default withStyles(styles)(AnimationPlayer);
